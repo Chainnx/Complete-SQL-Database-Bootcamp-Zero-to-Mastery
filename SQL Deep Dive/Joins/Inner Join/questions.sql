b7578aa6ee42d38681b141a7ff70940b111d0c55
Chainnx <61386736+Chainnx@users.noreply.github.com>
2023-01-24 16:42:52 +0800

    Update questions.sql

--- SQL Deep Dive/Joins/Inner Join/questions.sql
+++ SQL Deep Dive/Joins/Inner Join/questions.sql
@@ -6,7 +6,11 @@
 * ordered by orderid
 */
 
-
+SELECT b.*, a.state
+FROM customers as a
+INNER JOIN orders as b ON a.customerid = b.customerid
+WHERE a.state = 'OH' OR a.state ='NY' OR a.state ='OR'
+ORDER BY a.customerid ASC;
 
 /*
 * DB: Store
@@ -14,13 +18,23 @@
 * Question: Show me the inventory for each product
 */
 
+SELECT b.*, a.price, a.actor
+FROM products as a
+INNER JOIN inventory as b 
+ON a.prod_id = b.prod_id
+ORDER BY a.prod_id ASC;
+
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Show me for each employee which department they work in
 */
-
+SELECT a.emp_no, CONCAT(a.first_name, a.last_name) AS "FULL NAME", b.dept_no, c.dept_name
+FROM employees AS a
+INNER JOIN dept_emp as b ON a.emp_no = b.emp_no
+INNER JOIN departments as c ON b.dept_no = c.dept_no
+ORDER BY a.emp_no ASC;
 
 
 
