7e3a97b27b3e3cbeeeda30b4a93fe17eb3af9075
Chainnx <61386736+Chainnx@users.noreply.github.com>
2023-05-21 11:15:41 +0800

    Update questions.sql

--- SQL Deep Dive/Order By/questions.sql
+++ SQL Deep Dive/Order By/questions.sql
@@ -3,17 +3,22 @@
 * Table: employees
 * Question: Sort employees by first name ascending and last name descending
 */
-
+SELECT * from employees
+ORDER By first_name asc, last_name desc
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees by age
 */
-
+SELECT * from employees
+ORDER By age(birth_date) desc
 
 /*
 * DB: Employees
 * Table: employees
 * Question: Sort employees who's name starts with a "k" by hire_date
 */
+SELECT * from employees
+WHERE first_name ILIKE 'K%'
+ORDER By hire_date
