36e493fe6a655be24ab5de22189d5cacc86ad534
Chainnx <61386736+Chainnx@users.noreply.github.com>
2023-05-26 15:34:35 +0800

    Update exercises.sql

--- SQL Deep Dive/Grouping Sets/exercises.sql
+++ SQL Deep Dive/Grouping Sets/exercises.sql
@@ -3,10 +3,29 @@
 *  Database: Employees
 *  Table: Employees
 */
+SELECT de.dept_no as "departments",COUNT(e.emp_no)
+FROM employees as e
+INNER JOIN dept_emp as de ON e.emp_no = de.emp_no
+GROUP BY
+    GROUPING SETS(
+        (),
+        (de.dept_no)
+    )
+ORDER BY dept_no
 
 /*
 *  Calculate the total average salary per department and the total using grouping sets
 *  Database: Employees
 *  Table: Employees
 */
+SELECT de.dept_no, avg(s.salary)
+FROM employees as e
+INNER JOIN dept_emp as de ON e.emp_no = de.emp_no
+INNER JOIN salaries as s ON e.emp_no = s.emp_no
+GROUP BY 
+    GROUPING SETS (
+        (),
+        (de.dept_no)
+    )
+ORDER BY dept_no
 
