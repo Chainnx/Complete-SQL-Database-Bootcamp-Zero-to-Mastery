e92dd3881a201366047b53fbae86d64cadee6c7f
Chainnx <61386736+Chainnx@users.noreply.github.com>
2023-06-02 11:11:24 +0800

    Update questions.sql

--- SQL Deep Dive/NULLIF/questions.sql
+++ SQL Deep Dive/NULLIF/questions.sql
@@ -4,6 +4,12 @@
 * Question: Show NULL when the product is not on special (0)
 */
 
-SELECT *
+SELECT *, 
+CASE 
+    WHEN price > 20 THEN 'expensive'
+    WHEN price BETWEEN 10 AND 20 THEN 'AVERAGE'
+    WHEN price <= 10 THEN 'Cheap'
+END as "price class",
+NULLIF ("special", 0)
 FROM products
 
